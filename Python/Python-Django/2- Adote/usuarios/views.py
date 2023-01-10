7751043a38b549c4187c648075b7433a53c19377
NatanaelHLacerda <121961549+NatanaelHLacerda@users.noreply.github.com>
2023-01-10 00:04:14 -0300

    att 1.5

--- Python/Python-Django/2- Adote/usuarios/views.py
+++ Python/Python-Django/2- Adote/usuarios/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.contrib.auth import authenticate, login, logout
+
 
 
 def cadastro(request):
+    if request.user.is_authenticated:
+        return redirect('/divulgar/novo_pet')
+
     if request.method == "GET":
         return render(request, 'cadastro.html')
     elif request.method == "POST":
@@ -35,3 +40,29 @@ def cadastro(request):
             # Mensagem de Error!
             messages.add_message(request, constants.ERROR, 'Erro interno do sistema!')
             return render(request, 'cadastro.html')
+
+
+def logar(request):
+    if request.user.is_authenticated:
+        return redirect('/divulgar/novo_pet')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=nome, password=senha)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('/divulgar/novo_pet')
+
+            
+        else:
+            messages.add_message(request, constants.ERROR, 'Usuário ou senha incorreto!')
+            return render(request, 'login.html')
+
+
+def sair(request):
+    logout(request)
+    return redirect('/auth/login')        
