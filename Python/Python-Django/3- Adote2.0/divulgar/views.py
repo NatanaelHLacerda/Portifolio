9474675384a5c20768b0396fc5d1b2ce28adbfb4
NatanaelHLacerda <121961549+NatanaelHLacerda@users.noreply.github.com>
2023-01-12 14:43:21 -0300

    adote 2

--- Python/Python-Django/3- Adote2.0/divulgar/views.py
+++ Python/Python-Django/3- Adote2.0/divulgar/views.py
@@ -6,4 +6,35 @@ from .models import Tag, Raca, Pet
 def novo_pet(request):
     if request.method=="GET":
         tags = Tag.objects.all()
-        return render(request, 'novo_pet.html', {'tags': tags})
+        racas = Raca.objects.all()
+        return render(request, 'novo_pet.html', {'tags': tags, 'racas': racas})
+    elif request.method=="POST":
+        foto = request.FILES.get('foto')
+        nome = request.POST.get('nome')
+        descricao = request.POST.get('descricao')
+        estado = request.POST.get('estado')
+        cidade = request.POST.get('cidade')
+        telefone = request.POST.get('telefone')
+        tags = request.POST.getlist('tags')
+        raca = request.POST.get('raca')
+
+        # Dever de casa - Validar todos os dados
+
+        pet = Pet(
+            usuario=request.user,
+            foto=foto,
+            nome=nome,
+            descricao=descricao,
+            estado=estado,
+            cidade=cidade,
+            telefone=telefone,
+            raca_id=raca
+        )
+
+        pet.save()
+
+        for tag_id in tags:
+            tag=Tag.objects.GET(id=tag_id)
+            pet.tags.add(tag)
+
+        pet.save()       
