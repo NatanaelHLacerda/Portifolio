6cfbb9214f033cc763311f8b087cab08db515ee3
NatanaelHLacerda <121961549+NatanaelHLacerda@users.noreply.github.com>
2023-01-13 20:43:30 -0300

    a

--- Python/Python-Django/3- Adote2.0/adotar/urls.py
+++ Python/Python-Django/3- Adote2.0/adotar/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('listar_pets/', views.listar_pets, name="listar_pets")
+    path('', views.listar_pets, name="listar_pets")
 ]
