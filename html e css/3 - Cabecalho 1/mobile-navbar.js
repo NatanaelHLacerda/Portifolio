1b82dffa14f58121fdcadfe440d2a704fef7efa4
NatanaelHLacerda <121961549+NatanaelHLacerda@users.noreply.github.com>
2023-01-17 20:27:43 -0300

    add

--- html e css/3 - Cabecalho 1/mobile-navbar.js
+++ html e css/3 - Cabecalho 1/mobile-navbar.js
@@ -1,5 +1,5 @@
-class MobileNavbar{
-    constructor(mobileMenu, navList, navLinks) {
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks){
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
@@ -10,29 +10,26 @@ class MobileNavbar{
 
     animateLinks() {
         this.navLinks.forEach((link, index) => {
-            
             link.style.animation
             ? (link.style.animation = "")
-            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-                index / 7 + 0.3
-            }s`);
+            : (link.style.animation = `navLinkFade 0.5s ease
+               forwards ${
+                index / 7 + 0.3}s`);
         });
-
     }
 
     handleClick() {
         this.navList.classList.toggle(this.activeClass);
-        this.mobileMenu.classList.toggle(this.activeClass)
+        this.mobileMenu.classList.toggle(this.activeClass);
         this.animateLinks();
     }
 
-    addClickEvent() {
-        this.mobileMenu.addEventListener(
-            "click", this.handleClick);
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click", this.handleClick);
     }
 
-    init() {
-        if (this.mobileMenu) {
+    init(){
+        if (this.mobileMenu){
             this.addClickEvent();
         }
         return this;
@@ -45,4 +42,4 @@ const mobileNavbar = new MobileNavbar(
     ".nav-list li",
 );
 
-mobileNavbar.init()
+mobileNavbar.init();
