82e7674034ebdbe812a78002e2e9fad5e305a957
NatanaelHLacerda <121961549+NatanaelHLacerda@users.noreply.github.com>
2023-01-20 09:21:46 -0300

    add

--- Python/Python-Django/3- Adote2.0/usuarios/views.py
+++ Python/Python-Django/3- Adote2.0/usuarios/views.py
@@ -11,24 +11,28 @@ def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
     elif request.method == "POST":
-        nome = request.POST.get('nome')
+        login = request.POST.get('login')
+        #nome = request.POST.get('nome_completo')
         email = request.POST.get('email')
+        #celular = request.POST.get('celular')
+        #genero = request.POST.get('gender')
         senha = request.POST.get('senha')
-        confirmar_senha = request.POST.get('confirmar_senha')
+        
+        # confirmar_senha = request.POST.get('confirmar_senha')
 
-        if len(nome.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0 or len(confirmar_senha.strip()) == 0:
+        '''if len(primeiro_nome.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0 or len(confirmar_senha.strip()) == 0:
             messages.add_message(request, constants.ERROR, 'Preencha todos os dados!')
             return render(request, 'cadastro.html')
 
         if senha != confirmar_senha:
             messages.add_message(request, constants.ERROR, 'As senhas não conferem!')
-            return render(request, 'cadastro.html')
-
+            return render(request, 'cadastro.html')'''
         try:
-            user = User.objects.create_user(username=nome, email=email, password=senha)
-            messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso!')
-            return render(request, 'cadastro.html')
+            user = User.objects.create_user(username=login, email=email, password=senha)
+            #messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso!')
+            
         except:
+            return HttpResponse('Deu erro aqui')
             messages.add_message(request, constants.WARNING, 'Usuário já existente')
             return render(request, 'cadastro.html')
 
