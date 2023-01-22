e56108fa0674543f33d083ffc2b80e3c834062ca
NatanaelHLacerda <121961549+NatanaelHLacerda@users.noreply.github.com>
2023-01-22 13:21:16 -0300

    att 2

--- Python/Python-Django/1- Blog/blog/views.py
+++ Python/Python-Django/1- Blog/blog/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
+
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # três postagens em cada página
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Se a página não for um inteiro, exibe a primeira página
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Se a página estiver fora do intervalo,
+        # exibe a útima página de resultados
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'post/list.html', {'page':page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -12,4 +27,10 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, 'post/detail.html', {'post': post})
+    
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'post/list.html'
